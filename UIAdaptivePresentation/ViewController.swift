8cb291538db4678965e74a71d7ffc9b07e179ed8
Egor Ukolov <ukolovegor@yandex.ru>
2024-07-17 12:35:56 +0700

    Initial Commit

---
+++ UIAdaptivePresentation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIAdaptivePresentation
+//
+//  Created by Egor Ukolov on 17.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
