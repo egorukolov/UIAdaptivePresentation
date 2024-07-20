4412bba569067bb2c117318b2b9f0608af95fa34
Egor Ukolov <ukolovegor@yandex.ru>
2024-07-20 18:59:25 +0700

    Реализация сохранения введеных контактов через User Defaults

--- UIAdaptivePresentation/ViewControllers/NewContactViewController.swift
+++ UIAdaptivePresentation/ViewControllers/NewContactViewController.swift
@@ -7,13 +7,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+protocol NewContactViewControllerDelegate {
+    func saveContact(_ contact: String)
+}
+
+class NewContactViewController: UIViewController {
+    
+    @IBOutlet var doneButton: UIBarButtonItem!
+    @IBOutlet var firstNameTextField: UITextField!
+    @IBOutlet var lastNameTextField: UITextField!
+    
+    var delegate: NewContactViewControllerDelegate!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        doneButton.isEnabled = false 
+        
+        firstNameTextField.addTarget(
+            self,
+            action: #selector(firstNameTextFieldDidChanged),
+            for: .editingChanged)
+    }
+    
+    @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
+        saveAndExit()
+    }
+    
+    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        dismiss(animated: true
+        )
     }
 
+    
+    @objc private func firstNameTextFieldDidChanged() {
+        guard let firstName = firstNameTextField.text else { return }
+        doneButton.isEnabled = !firstName.isEmpty
+    }
+    
+    private func saveAndExit() {
+        
+        guard let firstName = firstNameTextField.text, !firstName.isEmpty else { return }
+        guard let lastName = lastNameTextField.text else { return }
+        let userFullName = "\(firstName) \(lastName)"
+        
+        delegate.saveContact(userFullName)
+        dismiss(animated: true)
+        
+    }
 
 }
 
